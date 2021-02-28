9cf33adc45303a584ce860d8bee61dec9d07e891
dd-dos <dungphamdang99@gmail.com>
2021-02-28 16:45:22 +0700

    fast commit

--- emotion_camtest.py
+++ emotion_camtest.py
@@ -8,30 +8,45 @@ import time
 import torch
 import logging
 
-from detection.retinaface.test_widerface import test_retina_face
-from PIL import Image
-from torchsummary import summary
-from inference import FaceAlignment
-from uuid import uuid4
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
 
-logging.getLogger().setLevel(logging.INFO)
+class emotion:
+    def __init__(self, model_path):
+        self.net = self.create_net()
+        self.net = load_model(model_path)
 
-def arg_parser():
-    P = argparse.ArgumentParser(description='landmark')
-    P.add_argument('--face-detector', type=str, required=True, help='face detector model to use')
-    P.add_argument('--modelfile', type=str, required=True, help='model file path')
-    P.add_argument('--detectmodelfile', type=str, required=True, help='face detect model file')
-    P.add_argument('--device', type=str, default='cpu', help='used device: cpu or cuda')
-    args = P.parse_args()
+    def create_net(self):
+        model = Sequential()
 
-    return args
-    
-def test_retina_landmark(args):
+        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+        model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(1024, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(7, activation='softmax'))
+
+        return model
+
+    def predict(self, img: np.ndarray) -> np.ndarray:
+        import ipdb; ipdb.set_trace()
+
+
+def test_retina_emotion(args):
     cap = cv2.VideoCapture(0)
-    fa = FaceAlignment(modelfilename=args.modelfile,
-                        device=args.device,
-                        face_detector=args.face_detector,
-                        facedetectmodelfile=args.detectmodelfile)
+    model = emotion(args.modelfile)
+
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -39,13 +54,21 @@ def test_retina_landmark(args):
         
         # frame = cv2.flip(frame, 0)
         key = cv2.waitKey(1) & 0xFF
-        time_0 = time.time()
-        frame = fa.draw_landmarks(frame)
-        # frame = fa.get_head_pose(frame)
-        logging.info("reference time: {}".format(time.time()-time_0))
-        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+        pred = model.predict(frame)
 
         cv2.imshow("", frame)
 
         if key == ord("q"):
             break
+
+def arg_parser():
+    P = argparse.ArgumentParser(description='emotion')
+    P.add_argument('--modelfile', type=str, required=True, help='model file path')
+    P.add_argument('--device', type=str, default='cpu', help='used device: cpu or cuda')
+    args = P.parse_args()
+
+    return args
+
+if __name__=="__main__":
+    args = arg_parser()
+    test_retina_emotion(args)
---
+++ scripts/emotion_camtest.sh
@@ -0,0 +1,2 @@
+python emotion_camtest.py --modelfile './cp/best.h5' \
+                          --device 'cpu'
