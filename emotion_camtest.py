a3659f25beee92a30c9d097875000001147d715f
dd-dos <dungphamdang99@gmail.com>
2021-03-01 09:57:47 +0700

    fast commit

--- emotion_camtest.py
+++ emotion_camtest.py
@@ -18,29 +18,7 @@ from retinaface.retina_detector import RetinaDetector
 
 class EmotionDetector:
     def __init__(self, model_path):
-        self.net = self.create_net()
         self.net = load_model(model_path)
-
-    def create_net(self):
-        model = Sequential()
-
-        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-        model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.25))
-
-        model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.25))
-
-        model.add(Flatten())
-        model.add(Dense(1024, activation='relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(7, activation='softmax'))
-
-        return model
 
     def predict(self, img: Image.Image):
         gray_img = img.convert('L')
