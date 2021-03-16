9825d42502abffb5f17751dabdd1e1d8bbb347e7
dd-dos <dungphamdang99@gmail.com>
2021-03-16 16:37:28 +0700

    fast commit

--- emotion.py
+++ emotion.py
@@ -13,12 +13,63 @@ import pandas as pd
 import shutil
 
 from PIL import Image
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import AveragePooling2D
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
 from retinaface.retina_detector import RetinaDetector
 import tensorflow_addons as tfa
+
+
+class EmotionDetectorDrake:
+    def __init__(self, model_path):
+        self.num_classes = 7
+        self.classes = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+        
+        # global sess
+        # # set_session(sess)
+        model = Sequential()
+
+        #1st convolution layer
+        model.add(Conv2D(64, (5, 5), activation='relu', input_shape=(48,48,1)))
+        model.add(MaxPooling2D(pool_size=(5,5), strides=(2, 2)))
+
+        #2nd convolution layer
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
+
+        #3rd convolution layer
+        model.add(Conv2D(128, (3, 3), activation='relu'))
+        model.add(Conv2D(128, (3, 3), activation='relu'))
+        model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
+
+        model.add(Flatten())
+
+        #fully connected neural networks
+        model.add(Dense(1024, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(1024, activation='relu'))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(self.num_classes, activation='softmax'))
+        
+        model.load_weights(model_path)
+        # model._make_predict_function()
+        self.model = model 
+
+    def predict(self, img: Image.Image):
+        gray_img = img.convert('L')
+
+        resized_img = gray_img.resize((48,48))
+        inp = np.array(resized_img).reshape((1,48,48,1))/255
+        inp = tf.constant(inp)
+
+        emotion_predictions = self.model.predict(inp)[0,:]
+
+        return np.argmax(emotion_predictions)
 
 
 class EmotionDetector:
@@ -29,7 +80,7 @@ class EmotionDetector:
         gray_img = img.convert('L')
 
         resized_img = gray_img.resize((48,48))
-        inp = np.array(resized_img).reshape((1,48,48,1))
+        inp = np.array(resized_img).reshape((1,48,48,1))/255
         inp = tf.constant(inp)
 
         pred = self.net(inp).numpy()[0]
@@ -86,48 +137,64 @@ def test_retina_emotion(args):
         if key == ord("q"):
             break
 
+
 def inference(args):
-    emotion_detector = EmotionDetector(args.modelfile)
+    try:
+        emotion_detector = EmotionDetector(args.modelfile)
+        print("using original emotion model!")
+    except:
+        emotion_detector = EmotionDetectorDrake(args.modelfile)
+        print("using drake emotion model")
+
     face_detector = RetinaDetector(device='cpu')
-    writer = pd.ExcelWriter('result.xlsx', engine='xlsxwriter')
+    try:
+        os.remove("test_result.txt")
+    except:
+        pass
+    root = os.path.join(args.root, "*")
+    for folder_path in tqdm.tqdm(glob.glob(root)):
+        label = folder_path.split("/")[-1]
+        counter = 0
+        total = 0
+        for idx, img_path in enumerate(glob.glob(os.path.join(folder_path, "*"))):
+            img = cv2.imread(img_path)
+            bboxes = face_detector.detect_from_image(img)
+            for box_idx, bbox in enumerate(bboxes):
+                if bbox[-1] >= 0.8:
+                    face = Image.fromarray(img).crop(bbox[:4])
+                    flag = emotion_detector.predict(face)
+                    if flag==0:
+                        emotion='angry'
+                    elif flag==1:
+                        emotion='disgusted'
+                    elif flag==2:
+                        emotion='fearful'
+                    elif flag==3:
+                        emotion='happy'
+                    elif flag==4:
+                        emotion='sad'
+                    elif flag==5:
+                        emotion='surprised'
+                    elif flag==6:
+                        emotion='neutral'
 
-    imgs_path = os.path.join(args.root, "*")
-    shutil.rmtree("./prediction", ignore_errors=True)
-    os.makedirs("prediction")
-    counter = 0
-    for img_path in tqdm.tqdm(glob.glob(imgs_path)):
-        img = cv2.imread(img_path)
-        bboxes = face_detector.detect_from_image(img)
-        for idx, bbox in enumerate(bboxes):
-            if bbox[-1] >= 0.8:
-                counter += 1
-                face = Image.fromarray(img).crop(bbox[:4])
-                flag = emotion_detector.predict(face)
-                if flag==0:
-                    emotion='angry'
-                elif flag==1:
-                    emotion='disgusted'
-                elif flag==2:
-                    emotion='fearful'
-                elif flag==3:
-                    emotion='happy'
-                elif flag==4:
-                    emotion='sad'
-                elif flag==5:
-                    emotion='surprised'
-                elif flag==6:
-                    emotion=='neutral'
+                    total += 1
+                    if emotion == label:
+                        counter += 1
+                    
+                    if args.save_result:
+                        cv2.imwrite(img, "{}.{}.{}.{}.jpg".format(label, emotion, idx, box_idx))
 
-                name = "./prediction/{}_{}.jpeg".format(emotion,counter)
-                cv2.imwrite(name, img)
-
+        with open("test_result.txt", "a") as f:
+            f.write("{}: {}\n".format(label, 100*(counter/total)))
 
 def arg_parser():
     P = argparse.ArgumentParser(description='emotion')
     P.add_argument('--job', type=str, default='camtest', help='do 1 of 2 jobs: camera test (camtest), processing images folder (inference)')
-    P.add_argument('--root', type=str, required=False, help='imgs folder path')
+    P.add_argument('--root', type=str, required=False, help='rooter path')
     P.add_argument('--modelfile', type=str, required=True, help='model file path')
     P.add_argument('--device', type=str, default='cpu', help='used device: cpu or cuda')
+    P.add_argument('--save-result', action='store_true')
     args = P.parse_args()
 
     return args
--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,4 +1,5 @@
 python emotion.py --modelfile './cp/best_12_03_2020.h5' \
                   --device 'cpu' \
                   --job "inference" \
-                  --root "./emotion"
+                  --root "emotion"
+                #   --save-result
